41f5e51d08fd8f492f43e00d54d7de9d0c1b7b2d
Radim SMILEK <radim.smilek@cz.abb.com>
2024-07-01 15:42:18 +0200

    Add project files.

---
+++ GlobalTicket.Admin.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GlobalTicket.Admin.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
